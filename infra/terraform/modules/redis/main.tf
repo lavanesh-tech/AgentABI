6179df879fd12eabc06f48ca8a642bc7b9593757
lavanesh <lavanesh645@gmail.com>
2026-09-18 09:53:54 -0400

    fix(security): complete terraform hardening

--- infra/terraform/modules/redis/main.tf
+++ infra/terraform/modules/redis/main.tf
@@ -25,11 +25,6 @@ resource "aws_vpc_security_group_ingress_rule" "redis_from_eks" {
   ip_protocol                  = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "redis_all" {
-  security_group_id = aws_security_group.redis.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-}
 
 data "aws_secretsmanager_secret_version" "auth_token" {
   count     = var.enable_auth_token ? 1 : 0
