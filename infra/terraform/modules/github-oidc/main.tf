b00235fa2d9f9e9a9c6b63c3f6b4a7e05fff814d
lavanesh <lavanesh645@gmail.com>
2026-09-19 16:39:40 -0400

    fix: allow Helm custom resource operations

--- infra/terraform/modules/github-oidc/main.tf
+++ infra/terraform/modules/github-oidc/main.tf
@@ -182,7 +182,7 @@ resource "aws_eks_access_policy_association" "deploy" {
 
   cluster_name  = var.eks_cluster_name
   principal_arn = aws_iam_role.deploy[0].arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type       = "namespace"
